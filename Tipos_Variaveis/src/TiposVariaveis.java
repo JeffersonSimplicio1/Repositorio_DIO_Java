223efd34ca8199835dd27ed6879e23e3e30df2e7
Smalls <jefferson.a.simplicio@gmail.com>
2024-07-15 22:53:00 -0300

    Aprendendo um pouco sobre operadores

--- Tipos_Variaveis/src/TiposVariaveis.java
+++ Tipos_Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+            String nomeCompleto = "Jefferson " + "Simplicio";
+            int idade = 30;
+            double altura = 1.59;
+        
+        System.out.println("Meu nome é " + nomeCompleto);
+        System.out.println("Eu tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura);
     }
 }
