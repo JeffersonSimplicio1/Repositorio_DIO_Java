164ad3cc2713db83261e92766f42d1e9d57b9347
Smalls <jefferson.a.simplicio@gmail.com>
2024-07-15 23:14:39 -0300

    trabalhando com boolean

---
+++ Tipos_Variaveis/src/Operadores2.java
@@ -0,0 +1,14 @@
+public class Operadores2 {
+    public static void main(String[] args) throws Exception{
+        String nomeUm = "Jefferson";
+        String nomeDois = "Jefferson";
+
+        boolean Resultado = (nomeUm == nomeDois);
+        
+
+    System.out.println("O nomeUm é igual ao nomeDois? " + Resultado);
+   
+        
+    }
+    
+}
