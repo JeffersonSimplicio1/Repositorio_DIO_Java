1f16be5bc57caa7ffa5fc2c44951ba3c230f55b8
Smalls <jefferson.a.simplicio@gmail.com>
2024-07-17 23:24:18 -0300

    Aprendendo a executar no cmd

---
+++ terminal/src/terminal.java
@@ -0,0 +1,11 @@
+public class terminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Oi, Eu sou o Goku!");
+        System.out.println("Meu nome é Jefferson =D");
+        System.out.println("Teenho 30 anos de idade");
+        System.out.println("Heeelp!!!");
+        
+
+    }
+}
